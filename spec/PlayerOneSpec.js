eb06a349057556a2a737bb878b2aa7368863c004
Louisa <louisaspicer@gmail.com>
2017-01-26 10:52:09 +0000

    Added test for PlayerOne to pick a type

---
+++ spec/PlayerOneSpec.js
@@ -0,0 +1,13 @@
+describe("Player One", function() {
+
+  var playerOne;
+
+  beforeEach(function() {
+    playerOne = new PlayerOne();
+  });
+
+  it("user can choose to be either the nought or the cross", function() {
+    playerOne.pickType(O);
+    expect(playerOne.type).toEqual(O);
+  });
+});
