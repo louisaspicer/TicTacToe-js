055e006cbb9a606ea625f75644671a61429d7b6f
Louisa <louisaspicer@gmail.com>
2017-01-26 11:03:35 +0000

    Added test for player one's inital turn to be true

--- spec/PlayerOneSpec.js
+++ spec/PlayerOneSpec.js
@@ -6,8 +6,16 @@ describe("Player One", function() {
     playerOne = new PlayerOne();
   });
 
-  it("initializes with null as the type", function() {
-    expect(playerOne.type).toEqual(null)
+  describe("Initializing", function() {
+
+    it("type starts as null", function() {
+      expect(playerOne.type).toEqual(null);
+    });
+
+    it("turn is true", function() {
+      expect(playerOne.turn).toEqual(true);
+    });
+
   });
 
   it("user can choose to be either the nought or the cross", function() {
