3993cb93f6f7fa6c92d435e6789b2d9a9a68ddfa
Louisa <louisaspicer@gmail.com>
2017-01-26 11:01:40 +0000

    Added a test for initializing playerOne with type null

--- spec/PlayerOneSpec.js
+++ spec/PlayerOneSpec.js
@@ -6,8 +6,13 @@ describe("Player One", function() {
     playerOne = new PlayerOne();
   });
 
+  it("initializes with null as the type", function() {
+    expect(playerOne.type).toEqual(null)
+  });
+
   it("user can choose to be either the nought or the cross", function() {
     playerOne.pickType("O");
     expect(playerOne.type).toEqual("O");
   });
+
 });
